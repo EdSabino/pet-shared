6193b4c3b31688f79d0f035540ad71ce565824d2
EdSabino <eduardoaikin@gmail.com>
2021-07-25 00:19:17 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const decorators = require('./decorators');
 
-exports.validationError = err => {
+const validationError = err => {
   const fields = {};
   const errors = Object.keys(err.errors);
   const messages = errors.map(e => { fields[e] = err.errors[e].properties.message; return fields[e]} );
