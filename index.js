f5696502fa102b6f87176d5c0b50a339a03b4f7a
EdSabino <eduardoaikin@gmail.com>
2021-07-25 00:11:29 -0300

    validation error

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const decorators = require('./decorators');
 
+exports.validationError = err => {
+  const fields = {};
+  const errors = Object.keys(err.errors);
+  const messages = errors.map(e => { fields[e] = err.errors[e].properties.message; return fields[e]} );
+  throw { success: false, message: messages.join(', '), error_fields: fields };
+}
+
 module.exports = {
-  decorators
+  decorators,
+  validationError
 }
