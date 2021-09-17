e938e1a2b009b7e41ba30bc99c1c48dc48e8d717
EdSabino <eduardoaikin@gmail.com>
2021-09-17 00:34:46 -0300

    change path

--- .build/indexOld.js
+++
@@ -1,7 +0,0 @@
-var decorators = require('./decorators');
-var validationError = require('./validation_error').validationError;
-module.exports = {
-    decorators: decorators,
-    validationError: validationError
-};
-//# sourceMappingURL=indexOld.js.map
--- .build/validation_error.js
+++
@@ -1,7 +0,0 @@
-exports.validationError = function (err) {
-    var fields = {};
-    var errors = Object.keys(err.errors);
-    var messages = errors.map(function (e) { fields[e] = err.errors[e].properties.message; return fields[e]; });
-    throw { success: false, message: messages.join(', '), error_fields: fields };
-};
-//# sourceMappingURL=validation_error.js.map
