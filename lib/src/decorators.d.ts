f8aaf82d48e140266fe79b8655831627e701ef18
EdSabino <eduardoaikin@gmail.com>
2021-09-17 21:32:11 -0300

    add lib

---
+++ lib/src/decorators.d.ts
@@ -0,0 +1,9 @@
+export declare function database(): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function isSuperAdmin(): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function hasPermission(permission: string, field: string): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function wrapper(): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function parseUser(): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function defaultCreate(hasFunction: boolean): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function defaultUpdate(hasFunction: boolean, name: string): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function defaultGet(name: string): (_: any, __: string | symbol, descriptor: any) => any;
+export declare function defaultList(name: string): (_: any, __: string | symbol, descriptor: any) => any;
